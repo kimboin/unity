11f54890941705a0b8a71b50e0d5c92cf6357e7b
김보인 <kimboin@gimboin-ui-MacBookAir.local>
2025-01-31 15:12:49 +0900

    360 폴더 추가

---
+++ 360/Assets/videoPlayManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+
+public class videoPlayManager : MonoBehaviour
+{
+    public GameObject Sphere;
+    VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = Sphere.GetComponent<VideoPlayer>();
+    }
+
+    public void PlayButton() {
+        videoPlayer.Play();
+    }
+    public void PauseButton()
+    {
+        videoPlayer.Pause();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
